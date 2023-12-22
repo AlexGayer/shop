c9952a83c2d1faede62ce89d1729ba2057ec5a29
Alexandre Gayer da Silva <alexandregs119@hotmail.com>
2023-12-22 11:55:10 -0300

    Http request

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,3 +1,3 @@
 class Constants {
-  static const productBaseUrl = 'https://shop-cod3r-1ec9e-default-rtdb.firebaseio.com/';
+  static const productBaseUrl = 'https://shop-cod3r-1ec9e-default-rtdb.firebaseio.com/products.json';
 }
